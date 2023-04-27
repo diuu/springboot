ef996b9d5490a4be9f2b934a06e0796be1a535f0
halo <872119367@qq.com>
2023-04-27 15:57:41 +0800

    test github menue use

--- src/main/java/com/javabase/day001/DateDemo1.java
+++ src/main/java/com/javabase/day001/DateDemo1.java
@@ -4,5 +4,6 @@ public class DateDemo1 {
     public static void main(String[] args) {
         System.out.println ("github");
         System.out.println ("gitee");
+        System.out.println ("run");
     }
 }
